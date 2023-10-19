8fbb8a8fc28d46346ba87257e3ad5116ad6a5ff3
Bugen Zhao <i@bugenzhao.com>
2023-10-19 17:53:37 +0800

    reorganize
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

---
+++ src/entities_extension.rs
@@ -0,0 +1,19 @@
+use sea_orm::ActiveValue::*;
+use sqlparser::ast;
+
+use crate::entities::my_column;
+
+impl my_column::ActiveModel {
+    pub fn from_ast(col: ast::ColumnDef, table_id: i32) -> Self {
+        my_column::ActiveModel {
+            id: NotSet,
+            name: Set(col.name.value.to_owned()),
+            table_id: Set(table_id),
+            data_type: Set(col.data_type.to_string()),
+            is_primary_key: Set(col
+                .options
+                .iter()
+                .any(|c| matches!(c.option, ast::ColumnOption::Unique { is_primary: true }))),
+        }
+    }
+}
---
+++ src/repl.rs
@@ -0,0 +1,27 @@
+use crate::session::Session;
+use anyhow::Result;
+
+pub async fn repl(session: &mut Session) -> Result<()> {
+    async fn handle_line(session: &mut Session, line: String) -> Result<()> {
+        let stmts =
+            sqlparser::parser::Parser::parse_sql(&sqlparser::dialect::GenericDialect {}, &line)?;
+        for stmt in stmts {
+            session.handle(stmt).await?;
+        }
+        Ok(())
+    }
+
+    let mut rl = rustyline::DefaultEditor::new()?;
+
+    loop {
+        let line = rl.readline(&if let Some(db_name) = session.current_db_name() {
+            format!("{}> ", db_name)
+        } else {
+            "> ".to_string()
+        })?;
+
+        if let Err(e) = handle_line(session, line).await {
+            tracing::error!("{:#}", e)
+        }
+    }
+}
